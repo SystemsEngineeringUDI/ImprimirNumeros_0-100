59b565ef92f7ec089ede8ae0b414f2a66948d1ee
Andromeda <26209570+jhonfv@users.noreply.github.com>
2018-02-21 21:32:34 -0500

    Add files via upload

---
+++ ImprimirNumeroscpp.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int suma=0;
+	do{
+		cout<<suma<<"\n";//se imprimi a cada siclo el numero indicado
+		suma=suma+1;//llevara la cuenta de cada ciclo(se incrementa uno a uno)
+	}while(suma<=100);//condicional que se repetira 100 veces 
+	return 0;
+}
